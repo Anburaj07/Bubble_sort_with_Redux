f1d6788f83599f09f48641dc16efa90ee1f13d01
Anburaj S <anburaj2203@gmail.com>
2024-01-09 10:58:19 +0530

    created basic structure

---
+++ src/components/InputForm.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const InputForm = () => {
+  return (
+    <DIV>
+        <form >
+            
+        </form>
+    </DIV>
+  )
+}
+
+export default InputForm
+
+const DIV=styled.div`
+    display: flex;
+    flex-direction: column;
+`
